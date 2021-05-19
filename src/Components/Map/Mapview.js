b72163771b9e22816c25ccc985636b7eb0ce3da9
Theerasak Phansai <theerasak_fusrodah@hotmail.com>
2021-05-20 02:14:30 +0700

    marker

--- src/Components/Map/Mapview.js
+++ src/Components/Map/Mapview.js
@@ -2,15 +2,9 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { Map, Marker, Popup } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './Mapview.css'
-import * as station from '../data/station.json'
+import station from '../data/station.json'
 
 const Mapview = () => {
-    const markerElement = station.features.map(data => {
-        const {
-            properties:{RRSTID,NAMET},
-            geometry :{coordinates}
-        } = data;
-    }); console.log(markerElement)
     const [mount, setMount] = useState(undefined)
     const initMap = useCallback(() => {
         const mapRender = new Map({
@@ -19,9 +13,38 @@ const Mapview = () => {
             center: [100.523186, 13.736717],
             zoom: 10
         });
-        
-   
-    
+        mapRender.on('load', function () {
+            mapRender.addSource('station', {
+                'type': 'geojson',
+                'data': station.features.map(stations => {
+        mapRender.addLayer({
+            'id' : stations.properties.RRSTID
+        });
+                    const marker = new Marker()
+                        .setLngLat([stations.geometry.coordinates[0], stations.geometry.coordinates[1]])
+                        .addTo(mapRender)
+                })
+            });
+        });
+
+        mapRender.on('click','RRSTID',function(e){station.features.map(stations => {
+            var coordinates = stations.geometry.coordinates.slice();
+            var description = stations.properties.NAMET;
+
+            while (Math.abs(e.lngLat.lng - coordinates) > 180) {
+                coordinates += e.lngLat.lng > coordinates ? 360 : -360;
+                }
+               new Popup()
+               .setLngLat([stations.geometry.coordinates[1],stations.geometry.coordinates[0]])
+               .setHTML(description)
+               .addTo(mapRender) 
+
+        })
+           
+        });
+
+
+
         setMount(mapRender);
     });
     useEffect(() => {
