229af8defa7f224e38cfb5bc791dc90bbfa84e16
Theerasak Phansai <theerasak_fusrodah@hotmail.com>
2021-06-01 00:53:24 +0700

    login commit

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,26 +1,41 @@
 import React,{useState,useContext} from 'react';
 import { Grid, Paper, Avatar, TextField, Button } from '@material-ui/core'
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import {Link} from 'react-router-dom';
+import {Link,useHistory} from 'react-router-dom';
 import {AutContext} from '../AutContext/AutContext.js'
+
 
 const Login = (props) => {
 
-    const [aut,setAut] = useContext(AutContext); 
+    const history = useHistory();
 
-    const [username,setUsername] =useState('');
-    const [password,setPassword]=useState('');
-    const [error,setError] =useState(null);
-    const [loading,setLoading] =useState(false);
+    const [aut,setAut] = useContext(AutContext); console.log(aut)
 
-    
+    const [username,setUsername] =useState(''); console.log(username)
+    const [password,setPassword]=useState('');  console.log(password)
 
+    //Login Style
     const paperStyle = { padding: 20, height: '50vh', width: 300, margin: "20px auto" }
     const avatarStyle ={backgroundColor:"red"}
     const buttonStyle ={marginTop:"20px"}
+
+    
+    
+
+    function loginSubmit(event){
+        
+            if(username == aut.username && password == aut.password)
+            {
+                history.push("/map")
+            }
+
+        event.preventDefault();
+        setAut();
+    }
+
     return (
         <Grid>
-            <Paper elevation={10} style={paperStyle}>
+            <Paper elevation={10} style={paperStyle} onSubmit={loginSubmit}>
                 <Grid align="center">
                     <Avatar style={avatarStyle}><LockOutlinedIcon /></Avatar>
                     <h2>Sing In</h2>
@@ -33,11 +48,8 @@ const Login = (props) => {
                 value={password}
                 onChange={e => setPassword(e.target.value)}
                 />
-                {error&& <Grid className="error">{error}</Grid>}
-                <Link to="/map" style={{textDecoration:"none"}}><Button style={buttonStyle} type="submit" color="primary" variant="contained" fullWidth
-                value={loading ? "Loading..." :"Login"}
-                disabled={loading}
-                >Sign In</Button></Link>
+                {/*error && <Grid className="error">{error}</Grid>*/}
+                <Button style={buttonStyle} type="submit" color="primary" variant="contained" fullWidth onSubmit={loginSubmit}>Sign In</Button>
             </Paper>
         </Grid>
     );
--- src/Components/Map/Mapview.js
+++ src/Components/Map/Mapview.js
@@ -3,6 +3,7 @@ import { Map, Marker, Popup } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css'
 import './Mapview.css'
 import station from '../data/station.json'
+import zIndex from '@material-ui/core/styles/zIndex';
 
 const Mapview = () => {
     const [mount, setMount] = useState(undefined)
@@ -13,37 +14,21 @@ const Mapview = () => {
             center: [100.523186, 13.736717],
             zoom: 10
         });
-        mapRender.on('load', function () {
-            mapRender.addSource('station', {
-                'type': 'geojson',
-                'data': station.features.map(stations => {
-        mapRender.addLayer({
-            'id' : stations.properties.RRSTID
+    mapRender.on('load',function(){
+        mapRender.addSource('station',{
+            'type': "geojson",
+            'data' : station.features.map(stations => {
+    const popup = new Popup({offset :25}).setText(
+        stations.properties.NAMET
+    );   
+    
+    const marker = new Marker()
+    .setLngLat([stations.geometry.coordinates[0],stations.geometry.coordinates[1]])
+    .setPopup(popup)
+    .addTo(mapRender);
+            })
         });
-                    const marker = new Marker()
-                        .setLngLat([stations.geometry.coordinates[0], stations.geometry.coordinates[1]])
-                        .addTo(mapRender)
-                })
-            });
-        });
-
-        mapRender.on('click','RRSTID',function(e){station.features.map(stations => {
-            var coordinates = stations.geometry.coordinates.slice();
-            var description = stations.properties.NAMET;
-
-            while (Math.abs(e.lngLat.lng - coordinates) > 180) {
-                coordinates += e.lngLat.lng > coordinates ? 360 : -360;
-                }
-               new Popup()
-               .setLngLat([stations.geometry.coordinates[1],stations.geometry.coordinates[0]])
-               .setHTML(description)
-               .addTo(mapRender) 
-
-        })
-           
-        });
-
-
+    })    
 
         setMount(mapRender);
     });
@@ -53,7 +38,12 @@ const Mapview = () => {
         }
     }, [mount, initMap])
     return (
-        <div id="map1" style={{ width: "100vw", height: "100vh" }}>
+        <div id="map1" style={{ width: "100vw", height: "100vh"}} className="map-option">
+            <ul className ="menu--nav">
+                <li>Home</li>
+                <li>About</li>
+                <li>Education</li>
+            </ul>
         </div>
     );
 };
